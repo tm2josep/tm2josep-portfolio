f46024138a2e2619925c05f6bd1770b4826b3a16
tm2josep <tmammenjoseph@gmail.com>
2024-11-13 20:20:13 -0400

    hopefully the last error fix today before I work on content

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "../components/layouts/main"
 import theme from "../theme/index"
 import { AnimatePresence } from "framer-motion"
-import { useEffect } from "react"
-import "../public/vanta.css"
 
 const Website = ({ Component, pageProps, router }) => {
-    useEffect(() => {
-        const threeScript = document.createElement("script")
-        threeScript.setAttribute("id", "threeScript")
-        threeScript.setAttribute(
-            "src",
-            "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"
-        )
-        document.getElementsByTagName("head")[0].appendChild(threeScript)
-        return () => {
-            if (threeScript) {
-                threeScript.remove()
-            }
-        }
-    }, [])
     return (
         <ChakraProvider theme={theme}>
             <Layout router={router}>
